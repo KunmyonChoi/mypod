20208d434f28fe0ea00919e7bea42ec0ad22ad25
Kunmyon Choi <dilly97@gmail.com>
2020-08-10 05:09:14 +0000

    Add fetch example code

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const fetch = require('node-fetch');
 
 const router = express.Router();
 
+const targetAddr = '127.0.0.1';
+const targetPort = 8080;
+
 router.get('/hello', (req, res) => {
-  res.send('world');
+  res.json({ message: 'world!', code: 0 });
+});
+
+router.get('/hi', async (req, res) => {
+  try {
+    await fetch(`https://${targetAddr}:${targetPort}/hello`, { method: 'GET' })
+      .then((ret) => ret.json())
+      .then((json) => res.json(json));
+  } catch (err) {
+    res.json({ code: -1, error: err });
+  }
 });
 
 module.exports = router;
